02b1d2fff5833518069338f6ef87abfcfc416c66
root <root@ru-dbmx1.yulsip.com>
2023-04-18 18:08:24 +0300

    Test for test

--- test/ecallmanager_test.exs
+++ test/ecallmanager_test.exs
@@ -2,7 +2,14 @@ defmodule EcallmanagerTest do
   use ExUnit.Case
   doctest Ecallmanager
 
-  test "greets the world" do
-    assert Ecallmanager.hello() == :world
+  test "pmap spawns async tasks" do
+    pmap([1,2,3], fn x -> x * 2 end)
+
+    assert_received({pid1, :ok})
+    assert_received({pid2, :ok})
+    assert_received({pid3, :ok})
+    refute pid1 == pid2
+    refute pid1 == pid3
+    refute pid2 == pid3
   end
 end
